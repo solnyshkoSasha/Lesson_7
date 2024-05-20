4ea4d6d89dcbd7e71986adb27bcbf5f6f1216fff
solnyshkoSasha <158512299+solnyshkoSasha@users.noreply.github.com>
2024-05-20 13:06:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let data = [5, 3, 6, 5.4, "Word", true]; // new Array(4, 5, 2);
+data[0] = "Some";
+console.log(data.length);
+
+let matrix = [
+  [5, 6],
+  ["World", false],
+  [true, 5.9, 0]
+];
+
+matrix[0][0] = 60;
+console.log(matrix[2][1]);
